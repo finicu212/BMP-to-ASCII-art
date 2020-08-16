42380e3bdebfedc3b8e00f0c8cbead91c0cdb829
finicu212 <smurfdeplastic@gmail.com>
2020-08-16 17:43:28 +0300

    added intensity character ramp

--- ASCII Art/ASCII Art.cpp
+++ ASCII Art/ASCII Art.cpp
@@ -6,6 +6,11 @@ int main()
     BMP image;
     image.ReadFromFile("photo.bmp");
 
+    // source: http://paulbourke.net/dataformats/asciiart/
+    const char *intensityRamp = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
+
+    std::cout << intensityRamp << "\n";
+
     std::cout << image.TellWidth() << ", " << image.TellHeight();
 
     return 0;
