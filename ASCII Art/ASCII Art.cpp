a652155ca2e91ad43d2464d67ddddb1b3bc4c9d6
finicu212 <smurfdeplastic@gmail.com>
2020-08-16 18:00:30 +0300

    get most intense color for each pixel

--- ASCII Art/ASCII Art.cpp
+++ ASCII Art/ASCII Art.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 #include "../includes/EasyBMP/EasyBMP.h"
 
+unsigned char getMostIntenseColor(RGBApixel pixel)
+{
+    unsigned char mostIntense = pixel.Red;
+    if (pixel.Blue > mostIntense)
+        mostIntense = pixel.Blue;
+    if (pixel.Green > mostIntense)
+        mostIntense = pixel.Green;
+
+    return mostIntense;
+}
+
 int main()
 {
     BMP image;
-    image.ReadFromFile("photo.bmp");
+    image.ReadFromFile("../input/photo1.bmp");
 
     // source: http://paulbourke.net/dataformats/asciiart/
     const char *intensityRamp = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
 
-    std::cout << intensityRamp << "\n";
+    for (int i = 0; i < image.TellWidth() - 1; i++)
+    {
+        for (int j = 0; j < image.TellHeight() - 1; j++)
+        {
+            RGBApixel currPixel = image.GetPixel(i, j);
 
-    std::cout << image.TellWidth() << ", " << image.TellHeight();
+            unsigned char mostIntense = getMostIntenseColor(currPixel);
+            std::cout << (int) mostIntense << "\n";
+        }
+    }
 
     return 0;
 }
